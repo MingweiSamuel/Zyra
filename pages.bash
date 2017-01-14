693dee9d9d95ad56e4a133b829bf18aeb26655b5
Mingwei Samuel <mingwei@berkeley.edu>
2017-01-13 22:35:21 -0800

    update pages.bash

--- pages.bash
+++ pages.bash
@@ -12,12 +12,17 @@ git init
 cat >index.md <<EOL
 ## ${project.build.finalName} build $CIRCLE_BUILD_NUM
 
+$(git log -1 | sed 's/^/    /')
+
 ### Docs
+
 * [Javadoc online](apidocs/)
 * [${project.build.finalName}-javadoc.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}-javadoc.jar)
 * [${project.build.finalName}-sources.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}-sources.jar)
 
+
 ### Jars
+
 * [${project.build.finalName}-standalone.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}-standalone.jar)
 * [${project.build.finalName}-standalone-full.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}-standalone-full.jar)
 * [${project.build.finalName}.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}.jar)
