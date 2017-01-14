56c8c61c168c2d6509a4aecded13a27786ce093e
Mingwei Samuel <mingwei@berkeley.edu>
2017-01-13 17:21:45 -0800

    updat gh-pages gen

--- pages.bash
+++ pages.bash
@@ -14,13 +14,13 @@ cat >index.md <<EOL
 
 ### Docs
 * [Javadoc online](apidocs/)
-* [${project.build.finalName}-javadoc.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/$CIRCLE_ARTIFACTS/${project.build.finalName}-javadoc.jar)
-* [${project.build.finalName}-sources.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/$CIRCLE_ARTIFACTS/${project.build.finalName}-sources.jar)
+* [${project.build.finalName}-javadoc.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}-javadoc.jar)
+* [${project.build.finalName}-sources.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}-sources.jar)
 
 ### Jars
-* [${project.build.finalName}-standalone.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/$CIRCLE_ARTIFACTS/${project.build.finalName}-standalone.jar)
-* [${project.build.finalName}-standalone-full.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/$CIRCLE_ARTIFACTS/${project.build.finalName}-standalone-full.jar)
-* [${project.build.finalName}.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/$CIRCLE_ARTIFACTS/${project.build.finalName}.jar)
+* [${project.build.finalName}-standalone.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}-standalone.jar)
+* [${project.build.finalName}-standalone-full.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}-standalone-full.jar)
+* [${project.build.finalName}.jar](https://circleci.com/api/v1/project/MingweiSamuel/Zyra/$CIRCLE_BUILD_NUM/artifacts/0/\$CIRCLE_ARTIFACTS/${project.build.finalName}.jar)
 
 EOL
 
