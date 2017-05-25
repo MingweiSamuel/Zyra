ac1d9f00d32662213751607663e353c18b26e13d
Mingwei Samuel <mingwei@berkeley.edu>
2017-05-24 22:08:38 -0700

    adding not null assertions to ApiComboFeaturedGamesSummonerCurrentGameTest

--- src/test/java/com/mingweisamuel/zyra/test/ApiComboFeaturedGamesSummonerCurrentGameTest.java
+++ src/test/java/com/mingweisamuel/zyra/test/ApiComboFeaturedGamesSummonerCurrentGameTest.java
@@ -14,6 +14,7 @@ import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -48,14 +49,20 @@ public class ApiComboFeaturedGamesSummonerCurrentGameTest extends ApiTest {
         api.spectator.getFeaturedGamesAsync(Region.NA).thenCompose(featured -> {
             checkFeatured(featured);
             CompletableFuture[] result = featured.gameList.stream().map(gameInfo -> {
+                assertNotNull(gameInfo);
+                assertNotNull(gameInfo.participants);
                 final Participant participant = gameInfo.participants.get(0);
                 return api.summoners.getBySummonerNameAsync(Region.NA, participant.summonerName)
-                    .thenCompose(summoner ->
-                        api.spectator.getCurrentGameInfoBySummonerAsync(Region.NA, summoner.id))
+                    .thenCompose(summoner -> {
+                        assertNotNull(summoner);
+                        return api.spectator.getCurrentGameInfoBySummonerAsync(Region.NA, summoner.id);
+                    })
                     .thenAccept(currentGame -> {
+                        assertNotNull(currentGame);
                         assertEquals(gameInfo.gameId, currentGame.gameId);
                         for (CurrentGameParticipant cParticipant : currentGame.participants) {
                             // check the summoner is in both queries
+                            assertNotNull(cParticipant);
                             if (participant.summonerName.equals(cParticipant.summonerName))
                                 return;
                         }
@@ -66,14 +73,22 @@ public class ApiComboFeaturedGamesSummonerCurrentGameTest extends ApiTest {
         }).get();
     }
     private void checkFeatured(FeaturedGames featured) {
+        assertNotNull(featured);
+        assertNotNull(featured.gameList);
         assertEquals(5, featured.gameList.size());
         for (FeaturedGameInfo gameInfo : featured.gameList) {
+            assertNotNull(gameInfo);
+            assertNotNull(gameInfo.participants);
             assertTrue(gameInfo.participants.size() >= 6);
             for (Participant player : gameInfo.participants) {
+                assertNotNull(player);
+                assertNotNull(player.summonerName);
                 assertTrue(player.summonerName.length() > 0);
                 assertFalse(player.bot);
             }
             assertTrue(gameInfo.gameId > 0);
+            assertNotNull(gameInfo.observers);
+            assertNotNull(gameInfo.observers.encryptionKey);
             assertTrue(gameInfo.observers.encryptionKey.matches("[a-zA-Z0-9/+]{32}"));
         }
     }
