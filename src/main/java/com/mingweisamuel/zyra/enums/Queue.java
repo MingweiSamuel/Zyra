6d0d2e0b25939225d1f0d3bf6fdc7c313e86b9b5
Nickypoo <hiamnicko@gmail.com>
2017-07-02 20:16:03 -0400

    Added Queue Types and re-organized a bit
    This needs to be evaluated.

--- src/main/java/com/mingweisamuel/zyra/enums/Queue.java
+++ src/main/java/com/mingweisamuel/zyra/enums/Queue.java
@@ -13,13 +13,24 @@ public class Queue {
     public static final String RANKED_SOLO_5x5 = "RANKED_SOLO_5x5";
     public static final String RANKED_TEAM_3x3 = "RANKED_TEAM_3x3";
     public static final String RANKED_TEAM_5x5 = "RANKED_TEAM_5x5";
+    public static final String RANKED_PREMADE_5x5 = "RANKED_PREMADE_5x5";
+    public static final String NORMAL_3x3 = "NORMAL_3x3";
+    public static final String NORMAL_5x5_BLIND = "NORMAL_5x5_BLIND";
+    public static final String NORMAL_5x5_DRAFT = "NORMAL_5x5_DRAFT";
     public static final String TEAM_BUILDER_DRAFT_RANKED_5x5 = "TEAM_BUILDER_DRAFT_RANKED_5x5";
     public static final String TEAM_BUILDER_DRAFT_UNRANKED_5x5 = "TEAM_BUILDER_DRAFT_UNRANKED_5x5";
     public static final String RANKED_FLEX_SR = "RANKED_FLEX_SR";
     public static final String RANKED_FLEX_TT = "RANKED_FLEX_TT";
     public static final String TEAM_BUILDER_RANKED_SOLO = "TEAM_BUILDER_RANKED_SOLO";
 
+    
+    
+    public static final String ASCENSION_5x5 = "ASCENSION_5x5";
+    public static final String ASSASSINATE_5x5 = "ASSASSINATE_5x5"
+    public static final String ARURF_5X = "ARURF_5X5";
     public static final String ARAM_5x5 = "ARAM_5x5";
+    public static final String BILGEWATER_5x5 = "BILGEWATER_5x5";
+    public static final String BILGEWATER_ARAM_5x5 = "BILGEWATER_ARAM_5x5";
     public static final String BOT_5x5 = "BOT_5x5";
     public static final String BOT_5x5_BEGINNER = "BOT_5x5_BEGINNER";
     public static final String BOT_5x5_INTERMEDIATE = "BOT_5x5_INTERMEDIATE";
@@ -28,24 +39,25 @@ public class Queue {
     public static final String BOT_TT_3x3 = "BOT_TT_3x3";
     public static final String BOT_URF_5x5 = "BOT_URF_5x5";
     public static final String CUSTOM = "CUSTOM";
+    public static final String DARKSTAR_3x3 = "DARKSTAR_3x3";
     public static final String FIRSTBLOOD_1x1 = "FIRSTBLOOD_1x1";
     public static final String FIRSTBLOOD_2x2 = "FIRSTBLOOD_2x2";
     public static final String GROUP_FINDER_5x5 = "GROUP_FINDER_5x5";
+    public static final String KING_PORO_5x5 = "KING_PORO_5x5";
     public static final String NIGHTMARE_BOT_5x5_RANK1 = "NIGHTMARE_BOT_5x5_RANK1";
     public static final String NIGHTMARE_BOT_5x5_RANK2 = "NIGHTMARE_BOT_5x5_RANK2";
     public static final String NIGHTMARE_BOT_5x5_RANK5 = "NIGHTMARE_BOT_5x5_RANK5";
     public static final String NORMAL_3x3 = "NORMAL_3x3";
-    public static final String NORMAL_5x5_BLIND = "NORMAL_5x5_BLIND";
-    public static final String NORMAL_5x5_DRAFT = "NORMAL_5x5_DRAFT";
     public static final String ODIN_5x5_BLIND = "ODIN_5x5_BLIND";
     public static final String ODIN_5x5_DRAFT = "ODIN_5x5_DRAFT";
     public static final String ONEFORALL_5x5 = "ONEFORALL_5x5";
-    public static final String RANKED_PREMADE_3x3 = "RANKED_PREMADE_3x3";
-    public static final String RANKED_PREMADE_5x5 = "RANKED_PREMADE_5x5";
+    public static final String ONEFORALL_MIRRORMODE_5x5 = "ONEFORALL_MIRRORMODE_5x5"
+    public static final String SIEGE = "SIEGE";
     public static final String SR_6x6 = "SR_6x6";
     public static final String URF_5x5 = "URF_5x5";
-    public static final String BILGEWATER_5x5 = "BILGEWATER_5x5";
-    public static final String BILGEWATER_ARAM_5x5 = "BILGEWATER_ARAM_5x5";
+    
+    
+    
 
     public static final String NONE = "NONE";
     public static final String NORMAL = "NORMAL";
@@ -53,6 +65,7 @@ public class Queue {
     public static final String ODIN_UNRANKED = "ODIN_UNRANKED";
     public static final String BOT_3x3 = "BOT_3x3";
     public static final String ARAM_UNRANKED_5x5 = "ARAM_UNRANKED_5x5";
+    public static final String DARKSTAR = "DARKSTAR";
     public static final String HEXAKILL = "HEXAKILL";
     public static final String CAP_5x5 = "CAP_5x5";
     public static final String URF = "URF";
@@ -63,6 +76,7 @@ public class Queue {
     public static final String COUNTER_PICK = "COUNTER_PICK";
     public static final String BILGEWATER = "BILGEWATER";
     public static final String SIEGE = "SIEGE";
+    
 
     /**
      * Olde specific values for stats playerStatSummaryType.
@@ -72,11 +86,13 @@ public class Queue {
 
         public static final String AramUnranked5x5 = "AramUnranked5x5";
         public static final String Ascension = "Ascension";
+        public static final String Arsr = "Arsr";
         public static final String Bilgewater = "Bilgewater";
         public static final String CAP5x5 = "CAP5x5";
         public static final String CoopVsAI = "CoopVsAI";
         public static final String CoopVsAI3x3 = "CoopVsAI3x3";
         public static final String CounterPick = "CounterPick";
+        public static final String Darkstar = "Darkstar";
         public static final String FirstBlood1x1 = "FirstBlood1x1";
         public static final String FirstBlood2x2 = "FirstBlood2x2";
         public static final String Hexakill = "Hexakill";
